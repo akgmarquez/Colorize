82c2edd619af022ca228ae591e4f4c9d1aecf304
EC2 Default User <ec2-user@ip-172-31-37-47.us-west-2.compute.internal>
2018-07-20 16:24:01 +0000

    hi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,37 @@
-require './config/environment'
-require './app/models/sample_model'
-
+require 'bundler'
+Bundler.require
+# require_relative 'models/model.rb'
+# require_relative 'models/ny_data_model.rb'
 class ApplicationController < Sinatra::Base
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get '/' do
-    return erb :index
+    erb :index
+  end
+
+  get '/random_page' do
+    erb :random_page
+  end
+
+  get '/content.erb' do
+    erb :content
+  end
+
+
+#   get '/results' do
+#     erb :results
+#   end
+
+  post '/results.erb' do
+    p params
+    person = Project.new(params)
+    @answer = person.final_results
+    p @answer
+    @final_array = person.get_results(@answer)
+    erb :results
   end
 end
